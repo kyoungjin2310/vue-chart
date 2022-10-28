734e3266a8a0b74686d67c71196abc8c7480d2da
kyoungjin2310 <kyoungjin2310@gmail.com>
2022-10-28 18:02:56 +0900

    feat:221028

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,61 @@
 <template>
-  <div></div>
+  <div>
+    <h2>Chart.js</h2>
+    <div class="chart">
+      <canvas id="myChart"></canvas>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import Chart from "chart.js/auto";
+export default {
+  mounted() {
+    //document.getElementById - 화면이 마운트 됐을때 써야 오류가 안생김
+    const ctx = document.getElementById("myChart");
+    // eslint-disable-next-line
+    const myChart = new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [
+          {
+            label: "# of Votes",
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)",
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)",
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+.chart {
+  width: 500px;
+}
+</style>
