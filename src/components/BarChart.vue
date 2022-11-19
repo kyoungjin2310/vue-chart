13b32a87c12bc4eac6d07052f9aa7ad23ea741f1
kyoungjin2310 <kyoungjin2310@gmail.com>
2022-11-19 23:44:19 +0900

    feat: chart 컴포넌트 모듈 제작

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -4,37 +4,16 @@
 
 <script>
 export default {
+  props: ['chartDataSet'],
   mounted() {
     //document.getElementById - 화면이 마운트 됐을때 써야 오류가 안생김
     const ctx = document.getElementById('myChart');
     // eslint-disable-next-line
     const myChart = new this.$_Chart(ctx, {
       type: 'bar',
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
       data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [
-          {
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)',
-            ],
-            borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)',
-            ],
-            borderWidth: 1,
-          },
-        ],
+        datasets: this.propsdata,
       },
       options: {
         scales: {
